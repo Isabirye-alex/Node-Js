2a3617f1f39bc6d99166ccee74663c6df194933d
Isabirye-alex <alexisabirye558@gmail.com>
2025-06-30 17:51:00 +0300

    step 16

--- backend_server/routes/brand.routes.js
+++ backend_server/routes/brand.routes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const { createBrand, getBrand, getBrandById, updateBrand, deleteBrand } = require('../controllers/brand.controllers.js');
-const upload = require('./config/brand.config.js');
+const upload = require('../config/brand.config.js');
 
 //Routes to handle all user funcions related to sub categories
 router.get('/', getBrand);    // GET /categories
