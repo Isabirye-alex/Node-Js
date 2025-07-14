0fc8c0670b0260c15de16db028a266fb741561cc
Isabirye-alex <alexisabirye558@gmail.com>
2025-07-14 07:02:27 +0300

    Monday

--- backend_server/controllers/review.controllers.js
+++ backend_server/controllers/review.controllers.js
@@ -9,7 +9,7 @@ async function postReview(req, res) {
         //Check if user has ever posted a review on that particular product
         const [user] = await db.query(`SELECT * FROM reviews WHERE user_id = ?`, [user_id]);
         if (user) {
-            const [response] = await db.query(`UPDATE reviews SET rating = ?, comment = ? WHERE user = ? and product_id=?`, [rating, comment, user_id, product_id]);
+            const [response] = await db.query('UPDATE reviews SET rating = ?, comment = ? WHERE user = ? and product_id=?', [rating, comment, user_id, product_id]);
             return res.status(201).json({ sucess: true, message: 'Review updated successfully', data: response });
         }
         //Post new review
@@ -31,9 +31,9 @@ async function editReview(req, res) {
         if (!user_id || !product_id || !rating || !comment) {
             return res.status(409).json({ success: false, message: 'Required fields are missing' });
         }
-        const [user] = await db.query(`SELECT * FROM reviews WHERE user_id = ?`, [user_id]);
+        const [user] = await db.query('SELECT * FROM reviews WHERE user_id = ?', [user_id]);
         if (user) {
-            const [response] = await db.query(`UPDATE reviews SET rating = ?, comment = ? WHERE user = ? and product_id=?`, [rating, comment, user_id, product_id]);
+            const [response] = await db.query('UPDATE reviews SET rating = ?, comment = ? WHERE user = ? and product_id=?', [rating, comment, user_id, product_id]);
             return res.status(201).json({ sucess: true, message: 'Review updated successfully', data: response});
         }
     } catch (error) {
@@ -48,7 +48,8 @@ async function getReviews(req, res) {
         const limit = 10;
         const offset = (page - limit);
         const { user_id } = req.body;
-        const [response] = await db.query(`(SELECT * FROM reviews WHERE user_id = ?) UNION ALL(SELECT * FROM reviews WHERE user_id!=? ORDER BY rating DESC LIMIT ? OFFSET)`, [user_id, user_id, limit, offset]);
+        const [response] = await db.query('(SELECT * FROM reviews WHERE user_id = ?) UNION ALL(SELECT * FROM reviews WHERE user_id!=? ORDER BY rating DESC LIMIT ? OFFSET)',
+            [user_id, user_id, limit, offset]);
         const hasUserReview = response.length > 0 && response[0].user_id === user_id;
         const userReview = hasUserReview ? response[0] : null;
         const otherReviews = hasUserReview ? response.slice(1) : response;
@@ -66,13 +67,20 @@ async function deleteReview(req, res) {
 
         }
         //check if has has posted a review yet
-        const [user] = await db.query(`SELECT * FROM reviews WHERE user_id = ?`, [user_id]);
+        const [user] = await db.query('SELECT * FROM reviews WHERE user_id = ?', [user_id]);
         if (user) {
-            const response = await db.query(`DELETE FROM reviews WHERE user_id = ?`, [user_id]);
+            const response = await db.query('DELETE FROM reviews WHERE user_id = ?', [user_id]);
             return res.status(201).json({ success: true, message: 'Review deleted successfully', data: response });
         }
      } catch (error) {
         return res.status(500).json({ success: false, message: 'Unknown error occurred', error: error.message });
     }
     
+}
+
+module.exports = {
+    deleteReview,
+    getReviews,
+    editReview,
+    postReview
 }
