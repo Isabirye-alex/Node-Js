caeffa7a7e0ad478ae3aa39d24b707e2ef989094
Isabirye-alex <alexisabirye558@gmail.com>
2025-07-24 17:09:24 +0300

    m

--- controllers/db.controller.js
+++ controllers/db.controller.js
@@ -1,23 +1,17 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'mysql-lexus-xshop.k.aivencloud.com',
-  user: 'avnadmin',
-  password: 'AVNS_TcIdKQ4M2YGw2nO2-W1',
+  host: process.env.AIVEN_HOST,
+  user: process.env.AIVEN_NAME,
+  password: process.env.AIVEN_PASSWORD,
   port: 26667,
-  database: 'defaultdb',
+  database: process.env.AIVEN_DB,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
 });
 
 // Enable async/await support
 const db = pool.promise();
 
 module.exports = db;
-
-
-//host = mysql-lexus-xshop.k.aivencloud.com
-//user = avnadmin
-//password = AVNS_TcIdKQ4M2YGw2nO2-W1
-//port = 26667
